60851a31d3bdba6f8d4842aa5713eb29833aafbe
Nikhil Singh <nikhils1220@gmail.com>
2026-08-13 08:05:46 -0700

    Finished multi-head self attention block

--- src/transformer_layers.py
+++ src/transformer_layers.py
@@ -9,12 +9,12 @@ class SelfAttentionHead(nn.Module):
         super().__init__()
 
         # dimensions of each token in the query, key, and value tensors
-        self.D_qkv = D / HEADS
+        self.D_qkv = D // HEADS
 
         # Q, K, and V transformations are purely linear
-        self.Q_linear_transform = nn.Linear(D, D_qkv, bias=False)
-        self.K_linear_transform = nn.Linear(D, D_qkv, bias=False)
-        self.V_linear_transform = nn.Linear(D, D_qkv, bias=False)
+        self.Q_linear_transform = nn.Linear(D, self.D_qkv, bias=False)
+        self.K_linear_transform = nn.Linear(D, self.D_qkv, bias=False)
+        self.V_linear_transform = nn.Linear(D, self.D_qkv, bias=False)
 
     def forward(self, x):
         # query, key, and value tensors
@@ -25,23 +25,33 @@ class SelfAttentionHead(nn.Module):
         # compute attention scores:
         # softmax(matmul(Q, transpose(K)) / sqrt(D_qkv))
         attention_scores = F.softmax(
-            (Q @ K.transpose(-2, -1)) / math.sqrt(self.D_qkv)
+            (Q @ K.transpose(-2, -1)) / math.sqrt(self.D_qkv),
+            dim=-1
         )
 
         # compute head output
         return attention_scores @ V
 
-    
-
 class MultiHeadSelfAttention(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.self_attention_head_1 = SelfAttentionHead()
-        self.self_attention_head_2 = SelfAttentionHead()
-        self.self_attention_head_3 = SelfAttentionHead()
-        self.self_attention_head_4 = SelfAttentionHead()
+        # list of self-attention heads
+        self.self_attention_heads = [SelfAttentionHead() for _ in range(HEADS)]
 
+        # apply linear transformation to vertically concatenated head outputs
+        self.output_linear_transform = nn.Linear(D, D, bias=False)
+
+    def forward(self, x):
+        # compute head outputs
+        head_outputs = [self_attention_head(x) for self_attention_head
+                        in self.self_attention_heads]
+
+        # vertically concatenate the head outputs
+        output = torch.cat(head_outputs, dim=-1)
+
+        # apply linear transformation
+        return self.output_linear_transform(output)
 
 class TransformerBlock(nn.Module):
     def __init__(self):
