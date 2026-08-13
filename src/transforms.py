65463558815a085d08a344fc57f4a38e9998afa2
Nikhil Singh <nikhils1220@gmail.com>
2026-08-13 11:29:37 -0700

    Added transformations for image preprocessing and augmentation

---
+++ src/transforms.py
@@ -0,0 +1,31 @@
+from torchvision.transforms import v2
+import torch
+
+CIFAR_MEAN = (0.49145, 0.48219, 0.44658)
+CIFAR_STD = (0.24687, 0.24332, 0.26137)
+
+crop_transform = nn.ModuleList([
+    v2.RandomCrop(
+        size=32,
+        padding=4
+    )
+])
+
+train_transforms = v2.Compose([
+    v2.RandomApply(crop_transform, p=0.8),
+    v2.RandomHorizontalFlip(p=0.5),
+    v2.ColorJitter(
+        brightness=[0.6, 1.4],
+        contrast=[0.6, 1.4],
+        saturation=[0.6, 1.4]
+    ),
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize(mean=CIFAR_MEAN, std=CIFAR_STD)
+])
+
+test_transforms = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize(mean=CIFAR_MEAN, std=CIFAR_STD)
+])
